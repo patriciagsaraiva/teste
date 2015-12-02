985e08b3449dfda5f26c38edae5ab4f70664a360
RubenDias <ruben.roque.dias@gmail.com>
2015-12-02 11:16:57 +0000

    Já com ligação ao Simulador e ao ISAGRAF

--- src/tcpMestre.java
+++ src/tcpMestre.java
@@ -1,7 +1,4 @@
 import java.net.*;
-import java.io.*;
-import java.util.*;
-import net.wimpi.modbus.*;
 import net.wimpi.modbus.msg.*;
 import net.wimpi.modbus.io.*;
 import net.wimpi.modbus.net.*;
@@ -14,89 +11,102 @@ import net.wimpi.modbus.util.*;
 public class tcpMestre {
 
     public static void main(String[] args) {
-        TCPMasterConnection con = null;
-        ModbusTCPTransaction trans = null;
-        ReadInputDiscretesRequest req_in = null;
-        ReadCoilsRequest req_coils = null;
-        ReadInputDiscretesResponse res_in = null;
-        ReadCoilsResponse res_coils = null;
 
-        InetAddress addr = null; //the slave's address
-        int port = Modbus.DEFAULT_PORT;
-        int ref = 0; //the reference; offset where to start reading from
-        int count = 0; //the number of DI's to read
-        int repeat = 1; //a loop for repeating the transaction
+        InetAddress addr = null;
+        int port_plant = 5502;
+        int port_plc = 5501;
+        int repeat = 60; //a loop for repeating the transaction
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < repeat; i++) {
             try {
-                //sending the actual Thread of execution to sleep X milliseconds
-                Thread.sleep(500);
+                Thread.sleep(250);
             } catch(InterruptedException ie) {}
+
             try {
                 //1. Setup the parameters
-                if (args.length < 3) {
-                    System.exit(1);
-                } else {
-                    try {
-                        String astr = args[0];
-                        int idx = astr.indexOf(':');
-                        if (idx > 0) {
-                            port = Integer.parseInt(astr.substring(idx + 1));
-                            astr = astr.substring(0, idx);
-                        }
-                        addr = InetAddress.getByName(astr);
-                        ref = Integer.decode(args[1]).intValue();
-                        count = Integer.decode(args[2]).intValue();
-                        if (args.length == 4) {
-                            repeat = Integer.parseInt(args[3]);
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                        System.exit(1);
-                    }
-                }
+                addr = InetAddress.getByName(args[0]);
 
-                //2. Open the connection
-                con = new TCPMasterConnection(addr);
-                con.setPort(port);
-                con.connect();
+                //2. Open the connections
+                TCPMasterConnection con_plant = tcpMestre.openConnection(addr,port_plant);
+                TCPMasterConnection con_plc = tcpMestre.openConnection(addr,port_plc);
+
+                con_plant.connect();
+
+                //3. Prepare the requests
 
 
-                //3. Prepare the request
-                req_in = new ReadInputDiscretesRequest(ref, count);
-                req_coils = new ReadCoilsRequest(0, 6);
-                //4. Prepare the transaction
-                trans = new ModbusTCPTransaction(con);
-                trans.setRequest(req_coils);
-
-                int k = 0;
-                do {
-                    trans.execute();
-                    res_coils = (ReadCoilsResponse) trans.getResponse();
-                    System.out.println("Coil Status = " + res_coils.getCoils());
-                    k++;
-                } while (k < repeat);
-
-                //5. Execute the transaction repeat times
-                trans.setRequest(req_in);
-                k = 0;
-                do {
-                    trans.execute();
-                    res_in = (ReadInputDiscretesResponse) trans.getResponse();
-                    System.out.println("Inputs Status = " + res_in.getDiscretes().toString());
-                    k++;
-                } while (k < repeat);
-                //------------------------------
-                //trans = new ModbusTCPTransaction(con);
 
 
-                //6. Close the connection
-                con.close();
+                //READING THE COILS (MOTORS)
+                BitVector res_coils = tcpMestre.readCoils(con_plant, 0, 6);
+                System.out.println("Coils Status = " + res_coils);
 
+                //READING THE INPUTS (SENSORS)
+                BitVector res_sensors = tcpMestre.readSensors(con_plant, 0, 3);
+                System.out.println("Inputs Status = " + res_sensors);
+                con_plant.close();
+
+                //READING THE VARIABLES (PLC)
+                ReadMultipleRegistersResponse res_variables = tcpMestre.readVariables(con_plc, 0, 2);
+                System.out.println("Variables Status = " + res_variables.getRegisterValue(0) + ", " + res_variables.getRegisterValue(1));
+                con_plc.connect();
+
+                con_plc.close();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
+    }
+
+    public static TCPMasterConnection openConnection (InetAddress address, int port) {
+        TCPMasterConnection con = new TCPMasterConnection(address);
+        con.setPort(port);
+        return con;
+    }
+
+    public static BitVector readCoils (TCPMasterConnection con, int ref, int count) {
+        ReadCoilsResponse res = null;
+        try {
+            ModbusTCPTransaction trans = new ModbusTCPTransaction(con);
+            ReadCoilsRequest req = new ReadCoilsRequest(ref, count);
+            trans.setRequest(req);
+            trans.execute();
+            res = (ReadCoilsResponse) trans.getResponse();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return res.getCoils();
+    }
+
+    public static BitVector readSensors (TCPMasterConnection con, int ref, int count) {
+        ReadInputDiscretesResponse res = null;
+        try {
+            ModbusTCPTransaction trans = new ModbusTCPTransaction(con);
+
+            ReadInputDiscretesRequest req = new ReadInputDiscretesRequest(ref, count);
+            trans.setRequest(req);
+            trans.execute();
+            res = (ReadInputDiscretesResponse) trans.getResponse();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return res.getDiscretes();
+    }
+
+    public static ReadMultipleRegistersResponse readVariables (TCPMasterConnection con, int ref, int count) {
+        ReadMultipleRegistersResponse res = null;
+        try {
+            ModbusTCPTransaction trans = new ModbusTCPTransaction(con);
+            ReadMultipleRegistersRequest req = new ReadMultipleRegistersRequest(ref, count);
+            req.setUnitID(2);
+
+            trans.setRequest(req);
+            trans.execute();
+            res = (ReadMultipleRegistersResponse) trans.getResponse();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return res;
     }
 }
 
