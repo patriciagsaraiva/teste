8320dd09749d22403b39c4d4763b1421cc4b8a6a
patriciasaraiva <patriciasaraiva14@hotmail.com>
2015-12-09 10:15:52 +0000

    nubgyv

--- src/udpEscravo.java
+++ src/udpEscravo.java
@@ -1,10 +1,12 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class udpEscravo {
+
 
     public static void main(String args[]) throws Exception {
         DatagramSocket serverSocket = new DatagramSocket(54321);
@@ -15,7 +17,12 @@ public class udpEscravo {
             System.out.println("Listening...");
             serverSocket.receive(receivePacket);
             String sentence = new String(receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
+            String b = new String();
+            for (int i = 0; i < 9; i++) {
+
+                b += sentence.charAt(i);
+            }
+            System.out.println("RECEIVED: " +b);
 
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
@@ -23,10 +30,32 @@ public class udpEscravo {
             sendData = capitalizedSentence.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
             serverSocket.send(sendPacket);
+            udpEscravo.decodeMessage(sentence);
         }
     }
 
-    public static void decodeMessage (String message, char trans, int no, int pIni, int pFin, int n) {
+    /*
+        carater-1 byte-trans
+        numero de ordem-3 bytes-no
+        peça de origem-1byte-pIni
+        peça final-1byte-pFin
+        quantidade a produzir-2bytes-n
+    */
+
+
+    public static void decodeMessage (String sentence /*, char trans, int no, int pIni, int pFin, int n*/) {
+
+
+        List<String> parts = new ArrayList<String>();
+
+        parts.add(sentence.substring(1,2));
+        parts.add(sentence.substring(2, 5));
+        parts.add(sentence.substring(5, 6));
+        parts.add(sentence.substring(6, 7));
+        parts.add(sentence.substring(7, 9));
+        String[] partsArray = parts.toArray(new String[0]);
+        System.out.println(parts);
+
 
     }
 }
--- src/udpMestre.java
+++ src/udpMestre.java
@@ -6,26 +6,32 @@ import java.net.InetAddress;
 
 
 
+
 public class udpMestre {
 
     public static void main(String args[]) throws Exception {
-        while(true) {
+
             BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
             DatagramSocket clientSocket = new DatagramSocket();
 
             InetAddress IPAddress = InetAddress.getByName("localhost");
             byte[] sendData = new byte[1024];
             byte[] receiveData = new byte[1024];
-            String sentence = inFromUser.readLine();
+            String sentence = ":T0012510";
             sendData = sentence.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 54321);
             clientSocket.send(sendPacket);
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             clientSocket.receive(receivePacket);
             String modifiedSentence = new String(receivePacket.getData());
-            System.out.println("FROM SERVER:" + modifiedSentence);
+            String a = new String();
+            for (int i = 0; i < 9; i++) {
+
+                    a +=modifiedSentence.charAt(i);
+            }
+            System.out.println("FROM SERVER:" + a);
             clientSocket.close();
-        }
+
     }
 
 }
