8fafae136afabec8df5503aad2ab22a8e5e9d857
RubenDias <ruben.roque.dias@gmail.com>
2015-12-09 11:07:32 +0000

    Correção da classe Order

--- src/Order.java
+++ src/Order.java
@@ -7,8 +7,8 @@ public class Order {
     char id;                            // piece id
     int NO;                             // piece order number
     int n;                              // quantity to produce
-    int PO, PB, P;                      // origin piece
-    int PF, PC;                         // final piece
+    Piece PO, PB, P;                    // origin piece
+    Piece PF, PC;                       // final piece
     int D;                              // destination
     String state;                       // state of the order (pending, processing, done
     int ready, processing, pending;     // number or pieces processed, in processing or pending
@@ -16,15 +16,15 @@ public class Order {
 
 
     public Order() {
-        char id = 0;
-        int NO = 0;
-        int n = 0;
-        int PO = 0, PB = 0, P = 0;
-        int PF = 0, PC = 0;
-        int D = 0;
-        String state = new String();
-        int ready = 0, processing = 0, pending = 0;
-        long entry = 0, init = 0, done = 0;
+        this.id = 0;
+        this.NO = 0;
+        this.n = 0;
+        this.PO = null; this.PB = null; this.P = null;
+        this.PF = null; this.PC = null;
+        this.D = 0;
+        this.state = "pendente";
+        this.ready = 0; this.processing = 0; this.pending = 0;
+        this.entry = 0; this.init = 0; this.done = 0;
     }
 }
 
